a98c3fa75687e0b69730f331ee8607df5e24bf9c
Sai Kaushik Soma <somakaushik98@gmail.com>
2023-08-01 16:31:37 -0700

    initial commit4

--- my_script.py
+++ my_script.py
@@ -1,1 +1,12 @@
-print("hello")
+# my_script.py - A simple Python script for testing GitHub Actions
+
+def main():
+    print("Hello, world!")
+    result = add_numbers(5, 10)
+    print("The result of adding 5 and 10 is:", result)
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    main()
