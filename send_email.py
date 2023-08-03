e195709be4233a6710f4a3a95d4ad356c4e762f9
Sai Kaushik Soma <somakaushik98@gmail.com>
2023-08-02 17:41:45 -0700

    initial commit11

--- send_email.py
+++ send_email.py
@@ -2,11 +2,14 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
+from google.oauth2 import service_account
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-SMTP_USERNAME = os.environ.get("GMAIL_USERNAME")
-SMTP_PASSWORD = os.environ.get("GMAIL_PASSWORD")
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
+CREDENTIALS_FILE = "service_account.json"
 FROM_EMAIL = "somakaushik98@gmail.com"
 TO_EMAIL = "somasaikaushik@gmail.com"
 
@@ -19,12 +22,15 @@ def send_email(output):
     body = f"The Python script completed. Here is the output:\n\n{output}"
     message.attach(MIMEText(body, "plain"))
 
-    # Send the email using SMTP
-    server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-    server.starttls()
-    server.login(SMTP_USERNAME, SMTP_PASSWORD)
-    server.sendmail(FROM_EMAIL, TO_EMAIL, message.as_string())
-    server.quit()
+    # Authenticate with Gmail using OAuth2
+    creds = service_account.Credentials.from_service_account_file(CREDENTIALS_FILE, scopes=SCOPES)
+
+    # Send the email using SMTP with OAuth2 credentials
+    with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+        server.starttls(context=creds)
+        server.login(FROM_EMAIL, creds)
+
+        server.sendmail(FROM_EMAIL, TO_EMAIL, message.as_string())
 
 if __name__ == "__main__":
     output = "This is the default output message."
--- test.dockerfile
+++ test.dockerfile
@@ -4,12 +4,18 @@ FROM python:3
 # Install mailutils for the mail command and other necessary packages
 RUN apt-get update && apt-get install -y --no-install-recommends mailutils
 
-# Set the Python script to copy into the image
+# Install Python libraries required for OAuth2 authentication with Gmail
+RUN pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client
+
+# Set the Python script and service account file to copy into the image
 COPY script.py /app/script.py
 COPY send_email.py /app/send_email.py
 
 # Set the working directory
 WORKDIR /app
+
+# Copy the service account file from GitHub secrets
+COPY service_account.json /app/service_account.json
 
 # Run the Python script when the container starts
 CMD ["python", "script.py"]
